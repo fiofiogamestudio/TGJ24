30ab83bc2d142c8e33d6a9e19192050e0bbdb2f6
fiofio <35527540+fiofiogamestudio@users.noreply.github.com>
2024-06-02 21:00:21 +0800

    update

---
+++ Assets/AppPhoto.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppPhoto : AppBase
+{
+    public GameObject PhotoPrefab;
+
+    public Transform PhotoRoot;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+}
